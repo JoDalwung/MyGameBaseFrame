9dbf8c3cc890724cc35987f1900c00d4ac7c6200
JoDalung <whekfdnd30@naver.com>
2024-05-24 16:47:36 +0900

    비동기 씬로드 로딩창 추가
    ActiveScene 함수에 로딩창 true/false 옵션추가

--- Assets/Scripts/0.Scene/0.MainScene/GameManager.cs
+++ Assets/Scripts/0.Scene/0.MainScene/GameManager.cs
@@ -8,16 +8,19 @@ public class GameManager : IScene
     // 어떤식으로 쓸지 미정
     public List<IScene.SceneName> SceneList;    
     //
-
     public IScene.SceneName EntryScene;
 
-    private void Awake()
+    protected override void _OnLoad()
     {
+        base._OnLoad();
         DontDestroyOnLoad(gameObject);
-        PlayerInfo.Instance.Name = "Player";     
+        PlayerInfo.Instance.Name = "Player";
     }
 
-    private void Start() => ActiveScene(EntryScene);
+    protected override void _OnLoadComplete()
+    {
+        ActiveScene(EntryScene , true);
+    }
 
     protected override void _OnEnter() => _AddEvent();
     protected override void _OnExite() => _RemoveEvent();
@@ -27,8 +30,6 @@ public class GameManager : IScene
 
 
     }
-
-
 
     void _RemoveEvent()
     {
--- Assets/Scripts/2.Common/IScene.cs
+++ Assets/Scripts/2.Common/IScene.cs
@@ -11,27 +11,68 @@ public class IScene : MonoBehaviour
         LobbyScene,       
     }
 
-    IEnumerator _cActiveScene(string SceneName)
+    RectTransform Loading_rect;
+    UnityEngine.UI.Text LoadProgress_txt;
+
+    IEnumerator _cActiveScene(string SceneName , bool Loading = true)
     {
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(SceneName);
+
+        asyncLoad.allowSceneActivation = !Loading;
+        float progress = 0;
+
+        if (Loading)
+        {
+            Loading_rect.gameObject.SetActive(true);
+        }
         while (!asyncLoad.isDone)
         {
-           // Debug.Log(asyncLoad.progress);
-            yield return null;
+            if (Loading)
+            {
+                progress = Mathf.MoveTowards(progress, asyncLoad.progress, Time.deltaTime);
+                LoadProgress_txt.text = $"{progress * 100}%";
+                if (progress >= 0.9f)
+                {
+                    // 로딩창 디자인에따라 커스텀
+                    LoadProgress_txt.text = "100%";
+                    //
+                    asyncLoad.allowSceneActivation = true;
+                    yield return new WaitForEndOfFrame();
+                    Loading_rect.gameObject.SetActive(false);
+                }
+                yield return null;
+            }
+            else 
+                yield return null;
         }        
     }
 
-    protected void ActiveScene(SceneName sceneName)
+    protected void ActiveScene(SceneName sceneName, bool Loading = true)
     {
-        StartCoroutine(_cActiveScene(sceneName.ToString()));
+        StartCoroutine(_cActiveScene(sceneName.ToString(), Loading));
     }
 
+
+
+
+    private void Awake() => _OnLoad();
+    private void Start() => _OnLoadComplete();
     private void OnEnable() => _OnEnter();
     private void OnDisable() => _OnExite();
 
+
+    protected virtual void _OnLoad()
+    {
+        Loading_rect = transform.GetChild(0).GetChild(1).GetComponent<RectTransform>();
+        LoadProgress_txt = Loading_rect.GetChild(1).GetComponent<UnityEngine.UI.Text>();
+        Loading_rect.gameObject.SetActive(false);
+    }
+
+    protected virtual void _OnLoadComplete()
+    {}
     protected virtual void _OnEnter()
-    { }
+    {}
     protected virtual void _OnExite()
-    { }
+    {}
 
 }
